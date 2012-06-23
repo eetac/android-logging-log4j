fc4bce2f0a53165e76607ae0e82c1cb035abbe0f
rolf.kulemann@gmail.com <rolf.kulemann@gmail.com>
2012-06-23 08:32:11 +0000

    fixed path

--- src/test/java/de/mindpipe/android/logging/log4j/example/ConfigureLog4J.java
+++ src/test/java/de/mindpipe/android/logging/log4j/example/ConfigureLog4J.java
@@ -15,6 +15,8 @@
 */  
 package de.mindpipe.android.logging.log4j.example;
 
+import java.io.File;
+
 import org.apache.log4j.Level;
 
 import android.os.Environment;
@@ -29,7 +31,7 @@ public class ConfigureLog4J {
 	public static void configure() {
 		final LogConfigurator logConfigurator = new LogConfigurator();
 		
-		logConfigurator.setFileName(Environment.getExternalStorageDirectory() + "myapp.log");
+		logConfigurator.setFileName(Environment.getExternalStorageDirectory() + File.separator + "myapp.log");
 		// Set the root log level
 		logConfigurator.setRootLevel(Level.DEBUG);
 		// Set log level of a specific logger
